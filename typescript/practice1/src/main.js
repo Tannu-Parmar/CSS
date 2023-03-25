17e74cbbd924f4972632bc0f9e7204fe9f1b7c7e
Tannu-Parmar <tannu22parmar@gmail.com>
2023-03-25 18:36:30 +0530

    home

---
+++ typescript/practice1/src/main.js
@@ -0,0 +1,5 @@
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+console.log(hex);
---
+++ typescript/practice2/src/main.ts
@@ -0,0 +1,43 @@
+// function padLeft(value: string, padding: any) {
+//   if (typeof padding === "number") {
+//     return Array(padding + 1).join(" ") + value;
+//   }
+//   if (typeof padding === "string") {
+//     return padding + value;
+//   }
+//   throw new Error(`Expected string or number, got '${typeof padding}'.`);
+// }
+ 
+// console.log("Hello world", 4);
+
+
+interface ErrorHandling {
+  success: boolean;
+  error?: { message: string };
+}
+ 
+interface ArtworksData {
+  artworks: { title: string }[];
+}
+ 
+interface ArtistsData {
+  artists: { name: string }[];
+}
+ 
+// These interfaces are composed to have
+// consistent error handling, and their own data.
+ 
+type ArtworksResponse = ArtworksData & ErrorHandling;
+type ArtistsResponse = ArtistsData & ErrorHandling;
+ 
+const handleArtistsResponse = (response: ArtistsResponse) => {
+  if (response.error) {
+    console.error(response.error.message);
+    return;
+  }
+ 
+  console.log(response.artists);
+};
+
+handleArtistsResponse()
+
